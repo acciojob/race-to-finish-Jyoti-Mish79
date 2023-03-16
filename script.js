794463c3bf49c7ed463c64654310a8e0e75eab47
Jyoti-Mish79 <jyotimishra2996@gmail.com>
2023-03-16 10:04:37 +0530

    jdgsyjs

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-window.promises = [];
+
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const promises = Array.from({ length: 5 }, () => Math.random() * 4000 + 1000)
+  .map((ms, i) => new Promise(resolve => setTimeout(() => resolve(`Promise ${i + 1} resolved`), ms)));
+
+Promise.any(promises)
+  .then(result => {
+    document.getElementById('output').innerText = result;
+  });
